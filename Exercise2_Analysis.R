3ba294c91c946b018d1ebdd0fd05d23d6d7465c0
Henrik Husberg <henrik.husberg@helsinki.fi>
2017-01-27 22:51:19 +0200

    Excercise 2 pretty much done

--- Exercise2_Analysis.R
+++ Exercise2_Analysis.R
@@ -13,3 +13,20 @@ library(GGally)
 #Plot matrix for all variables
 p <- ggpairs(learning2014, mapping = aes(col = gender, alpha = 0.3), lower = list(combo = wrap("facethist", bins = 20)))
 p
+
+#Run multiple regressions regressing attitude, deep and surf on points
+
+model1 <- lm(points ~ attitude + deep + surf, data = learning2014)
+summary(model1)
+
+#Run model with only significant regressors included
+
+model2 <- lm(points ~ attitude, data = learning2014)
+summary(model2)
+
+#Plot the residuals to see if model is valid
+par(mfrow = c(2,2))
+plot(model2)
+
+#Plot the regression line and interpret results
+qplot(attitude, points, data = learning2014) + geom_smooth(method = "lm")
