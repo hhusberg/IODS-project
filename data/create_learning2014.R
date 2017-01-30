e13dea894a10d42a08acd15a84de4f1e07149f67
Henrik Husberg <henrik.husberg@helsinki.fi>
2017-01-30 19:24:50 +0200

    Updated order to match assignment

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -1,9 +1,9 @@
 #Henrik Husberg, 26.01 2017. Data wrangling part 1.
 
-#Read data from web.
+#Read data from web (step 2, 1p).
 lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
 
-#Examine structure and dimension
+#Examine structure and dimension (step 2, 1p)
 dim(lrn14)
 str(lrn14)
 lrn14
@@ -12,7 +12,7 @@ lrn14
 #Some values seem to range from 1 to 5, most probably scores for tasks.
 #Some values seem to be sums of items: attitude, points.
 
-#Exclude observations with 0 points 
+#Exclude observations with 0 points (step 3, 1p)
 lrn14noz <- subset(lrn14, (Points > 0))
 lrn14noz
 dim(lrn14noz)
@@ -21,7 +21,7 @@ dim(lrn14noz)
 install.packages("dplyr")
 library(dplyr)
 
-#Select and sum items for deep, stra and surf
+#Select and sum items for deep, stra and surf (step 3, 1p)
 deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30", "D06",  "D15", "D23", "D31")
 deep_columns <- select(lrn14noz, one_of(deep_questions))
 deep_columns
@@ -52,18 +52,18 @@ my_dataframe
 
 #Looking good - tho maybe not the most elegant coding...
 
-#Now set working directory to IODS folder
+#Now set working directory to IODS folder (step 4, 3p)
 setwd("~/GitHub/IODS-project")
 
-#...and save the file
+#...and save the file (step 4, 3p)
 write.csv(my_dataframe, file = "./data/learning2014.csv", row.names = FALSE)
 
-#...then read it and weep
+#...then read it and weep (step4, 3p)
 read.csv("./data/learning2014.csv")
 
 
 
-#Compare dataframes just to be sure
+#Compare dataframes just to be sure (step 4, 3p)
 my_dataframe_test <- read.csv("./data/learning2014.csv")
 summary(my_dataframe)
 summary(my_dataframe_test)
